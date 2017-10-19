44a862287c9cb33cf6cdcd9989290d499aeb5534
AliDi <a.dinsen@hotmail.fr>
2017-10-19 18:17:56 +0200

    maj debruitage

--- CODES/src/CSMReconstruction.m
+++ CODES/src/CSMReconstruction.m
@@ -1,15 +1,19 @@
-function [d1] = CSMReconstruction( Spp , M)
+function [Spp_out , d1] = CSMReconstruction( Spp)
 %calculates diagonal elements d to reduce noise on the diagonal of Spp, kipping Spp hermitian nonnegative semi-definite.
 %M is the number of microphones (Spp : (MxM))
 %d1 : (M x 1)
 %from Hald 2016
+
+M=size(Spp,1);
 CSM=double(Spp);
 M=double(M);
-cvx_begin quiet
+cvx_begin 
 	variable d1(M);
 	CSM + diag(d1) == hermitian_semidefinite(M);
 	minimize( sum(d1) );
 cvx_end
+
+Spp_out = Spp+diag(d1);
 
 %which strictly equivalent to Dougherty 2016
 %cvx_begin
--- CODES/src/OrthogonalBeamforming.m
+++ CODES/src/OrthogonalBeamforming.m
@@ -27,19 +27,32 @@ function [Sqq_diag , W , Nsrc] = OrthogonalBeamforming(Spp , G_map_mic , mic_wei
     V = V(:, ind);
     
     
-    Lambda=diag(Lambda); 
+ 
     
     figure
-    stem(diag(Lambda))
+    stem(Lambda)
     title('Eigenvalues of $S_{pp}$')
+    l(1)=Lambda(1);
+    for i=2:M
+		l(i)=l(i-1)+Lambda(i);
+	end
+	sum(Lambda)
+    figure
+    stem(l)
+    ylabel('%')
+    xlabel('N')    
+    title('Énergie en % portée par la somme des N premières valeurs propres')
+    
     Nsrc = input('Number of sources ? ');
        
     Lambda = abs(real(Lambda));
-    
+    Lambda=diag(Lambda);
 
     
     for i=1:Nsrc
     	Spp_i = V(:,i) * Lambda(i ,i ) * V(:,i)';
+    	%figure
+    	%imagesc(real(Spp_i))
     	
     	for n=1:Nmap
             %%% Calculates weighting vectors (source scaling)
